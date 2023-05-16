8d53f56df0590b88c0a5d224f383489c4bff5663
charles-tech <charles.fernando.miranda@gmail.com>
2023-05-16 10:07:57 -0300

    basic

---
+++ config/config.php
@@ -0,0 +1,98 @@
+
+<!DOCTYPE html>
+<html lang="pt -BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Charles Dev</title>
+</head>
+
+<body>
+<header>
+    <nav class="#f48fb1 indigo lighten-3">
+
+        <a href="#" data-target="slide-out" class="sidenav-trigger">
+            <i class="material-icons">menu</i>
+        </a>
+        <div class="nav-wrapper container">
+            <!-- logo -->
+            <div class="brand-logo light">Charles Dev</div>
+            <!-- menu Desketop -->
+            <ul class="right hide-on-med-and-down">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#produto">Portfólio</a></li>
+                <li><a href="#contato">Contato</a></li>
+                <li><a href="restrito.php">Restrito</a></li>
+            </ul>
+
+            <!-- menu mobile -->
+            <ul id="slide-out" class="sidenav">
+
+                <li><a href="index.php"> <i class="material-icons left">home</i>Home</a></li>
+                <li><a href="#produto"> <i class="material-icons left">favorite</i>Portfólio</a></li>
+                <li><a href="#contato"> <i class="material-icons left">call</i>Contato</a></li>
+                <li><a href="restrito.php"> <i class="material-icons left">network_locked</i>Restrito</a></li>
+            </ul>
+
+
+
+        </div>
+
+    </nav>
+
+
+</header>
+
+<p>&nbsp;</p>
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script src="../php-mvcr/src/js/jquery.js"></script>
+
+</body>
+
+<footer class="page-footer indigo lighten-3">
+    <div class=" container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">Localização</h5>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3576.443630252892!2d-48.855550823816586!3d-26.31213067701034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94deb044cfe71483%3A0x7e1184e6aa153dae!2sR.%20Henrique%20Meyer%2C%20231%20-%20Anita%20Garibaldi%2C%20Joinville%20-%20SC%2C%2089203-570!5e0!3m2!1spt-BR!2sbr!4v1684173578314!5m2!1spt-BR!2sbr" width="350" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Redes Sociais</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3" href="https://github.com/charles-tech" target="_blank"><img src="../php-mvcr/src/img/github.png" height="27" width="27" alt="">
+                            GitHub</a></li><br>
+                    <li><a class="grey-text text-lighten-3" href="https://www.facebook.com/charles.miranda.144" target="_blank"><img src="../php-mvcr/src/img/facebook.png" alt="">
+                            Facebook</a></li><br>
+                    <li><a class="grey-text text-lighten-3" href="https://www.instagram.com/charles_miranda31/"target="_blank"><img src="../php-mvcr/src/img/instagram.png" alt="">
+                            Instagram</a></li><br>
+                    <li><a class="grey-text text-lighten-3" href="https://www.linkedin.com/in/charles-miranda-9a3b0215b/"target="_blank"><img src="../php-mvcr/src/img/linkedin.png" alt="">
+                            Linkdim</a></li><br>
+                    <li><a class="grey-text text-lighten-3"  href="https://wa.me/5547991460643/" ><img src="../php-mvcr/src/img/whatsapp.png"  alt="">
+                            WhatsApp</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            © 2023 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">Charles Dev</a>
+        </div>
+    </div>
+</footer>
+
+</html>
---
+++ src/js/jquery.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function () {
+    $(".sidenav").sidenav();
+    $(".slider").slider();
+    $(".materialboxed").materialbox();
+    $(".modal").modal();
+    $(".parallax").parallax();
+    $(".parallax").parallax();
+});
